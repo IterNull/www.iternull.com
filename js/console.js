a3a6b248cf225af1193a18ba45bf2376d699dccb
ZMOM1031 <zhujiandong1031@hotmail.com>
2016-02-18 14:57:05 +0800

    First Commit

---
+++ js/console.js
@@ -0,0 +1,17 @@
+if (window.console) {
+    var cons = console;
+    if (cons) {
+        cons.log("%c \n\
+ ___ _            _   _       _ _\n\
+|_ _| |_ ___ _ __| \\ | |_   _| | |\n\
+ | || __/ _ \\ '__|  \\| | | | | | |\n\
+ | || ||  __/ |  | |\\  | |_| | | |\n\
+|___|\\__\\___|_|  |_| \\_|\\__,_|_|_|\n\
+\n\
+By ZMOM1031\n\
+http://www.iternull.com/\n\
+\n\
+","color: #00adef;");
+        
+    }
+}
